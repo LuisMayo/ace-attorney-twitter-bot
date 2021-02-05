bf6f95da2535561673f6f16d08fbfda1c5a547dd
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2021-02-05 13:45:56 +0100

    Avoid blank messages

--- comment_list_brige.py
+++ comment_list_brige.py
@@ -2,6 +2,8 @@ class Comment:
   def __init__(self, tweet):
     self.author = Author(tweet.user.name)
     self.body = tweet.full_text
+    if (len(self.body) == 0):
+        self.body = '...'
     self.score = 0
 
 class Author:
