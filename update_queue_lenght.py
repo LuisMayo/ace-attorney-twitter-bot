77f162a79e79a30190c6c66b24e87191d153c1e9
LuisMayo <luismayovalbuena@outlook.es>
2022-01-25 11:52:49 +0100

    Fixed queue display

--- update_queue_lenght.py
+++ update_queue_lenght.py
@@ -3,11 +3,10 @@ from time import sleep
 
 def update_queue_length(params):
     while True:
-        if params['last_time'] is None:
+        if params['queue'].empty():
             params['api'].update_profile(location='queue: empty')
         else:
             time_difference = str(datetime.now(timezone.utc) - params['last_time'])
-            params['last_time'] = None
             size = params['queue'].qsize()
             # We only need hour and minutes
             time_difference_formatted = time_difference[:time_difference.index(':', 3)]
